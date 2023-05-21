122f25be892a3618802d621bb9649de77a07f51b
aryasuneesh <aryasuneesh3@gmail.com>
2023-05-21 17:32:11 +0530

    Initial submission

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# culturecove
+![Logo](https://media.discordapp.net/attachments/932599780411076620/1109812477711093812/cclogof.png?width=1440&height=413)
+
+<p align="center" style="font-size:240px;">
+<b>Explore the lesser-known places of beauty and wonder.</b>
+</p>
+
+<p align="center">
+  <img src="https://skillicons.dev/icons?i=py,github">
+</p>
+
--- map.py
+++ map.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import streamlit.components.v1 as components
 import folium
 import json
 
@@ -8,8 +7,6 @@ MARKER_ICON = folium.Icon(icon="info-sign", prefix="fa")
 PLACES_FILE = "places.json"
 
 # Helper function to load places from JSON file
-
-
 def load_places():
     try:
         with open(PLACES_FILE, "r") as f:
@@ -18,25 +15,21 @@ def load_places():
         return []
 
 # Helper function to save places to JSON file
-
-
 def save_places(places):
     with open(PLACES_FILE, "w") as f:
         json.dump(places, f)
 
 # Helper function to mark a place on the map
-
-
 def mark_place_on_map(map_obj, latitude, longitude, place_name, description, reviews):
     # Create marker with place name and description
     popup_content = f"<b>{place_name}</b><br>{description}<br>"
 
-    # if reviews:
-    #     reviews_content = "<ul>"
-    #     for review in reviews:
-    #         reviews_content += f"<li>{review}</li>"
-    #     reviews_content += "</ul>"
-    #     popup_content += reviews_content
+    if reviews:
+        reviews_content = "<ul>"
+        for review in reviews:
+            reviews_content += f"<li>{review}</li>"
+        reviews_content += "</ul>"
+        popup_content += reviews_content
 
     # Add marker to map
     folium.Marker(
@@ -46,12 +39,8 @@ def mark_place_on_map(map_obj, latitude, longitude, place_name, description, rev
     ).add_to(map_obj)
 
 # Main function to build the Streamlit app
-
 def main():
-    st.image("images\cclogof.png")
-    # st.title(":orange[CultureCove]:world_map:")
-    st.subheader("_Explore the lesser-known places of beauty and wonder._")
-    st.divider()
+    st.title("Hidden Places of Art and Culture")
 
     # Load existing places from JSON file
     existing_places = load_places()
@@ -71,24 +60,17 @@ def main():
         )
 
     # Display the map in the main page
-    st.subheader("Map")
+    st.header("Map")
     folium_map = m._repr_html_()
-    st.components.v1.html(folium_map, width=700, height=600, scrolling=True)
-
-    # # Generate HTML for the map
-    # map_html = m.get_root().render()
-
-    # # Display the map using iframe
-    # st.header("Map")
-    # components.html(map_html, width=800, height=600, scrolling=False)
+    st.components.v1.html(folium_map, width=800, height=600, scrolling=True)
 
     # Create a sidebar for adding a place and writing a review
-    st.sidebar.title(":orange[Actions]")
+    st.sidebar.title("Actions")
     action = st.sidebar.radio("Select Action", ["Add Place", "Write Review"])
 
     # Add Place action
     if action == "Add Place":
-        st.sidebar.subheader(":orange[Add Place]")
+        st.sidebar.subheader("Add Place")
         place_name = st.sidebar.text_input("Place Name")
         latitude = st.sidebar.number_input("Latitude")
         longitude = st.sidebar.number_input("Longitude")
@@ -103,13 +85,12 @@ def main():
             }
             existing_places.append(place_data)
             save_places(existing_places)
-            mark_place_on_map(m, latitude, longitude,
-                              place_name, description, [])
+            mark_place_on_map(m, latitude, longitude, place_name, description, [])
             st.sidebar.success("Place added successfully!")
 
     # Write Review action
     elif action == "Write Review":
-        st.sidebar.subheader(":orange[Write Review]")
+        st.sidebar.subheader("Write Review")
         place_options = [place["place_name"] for place in existing_places]
         selected_place = st.sidebar.selectbox("Select a Place", place_options)
         review_text = st.sidebar.text_area("Review")
@@ -134,7 +115,6 @@ def main():
                 st.write(review)
         else:
             st.write("No reviews yet for this place.")
-
 
 if __name__ == "__main__":
     main()
